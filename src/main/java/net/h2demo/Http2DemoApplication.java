4c46df3427eebca38fb9ed60ad37769ff2eb2ee6
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2017-11-26 13:07:20 +0900

    h2 upgrade 관련 선언 추가

--- src/main/java/net/h2demo/Http2DemoApplication.java
+++ src/main/java/net/h2demo/Http2DemoApplication.java
@@ -1,7 +1,11 @@
 package net.h2demo;
 
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Http2DemoApplication {
@@ -10,4 +14,15 @@ public class Http2DemoApplication {
 		SpringApplication.run(Http2DemoApplication.class, args);
 	}
 
+	@Bean
+	public EmbeddedServletContainerCustomizer tomcatCustomizer() {
+		return (container) -> {
+			if (container instanceof TomcatEmbeddedServletContainerFactory) {
+				((TomcatEmbeddedServletContainerFactory) container)
+						.addConnectorCustomizers((connector) -> {
+							connector.addUpgradeProtocol(new Http2Protocol());
+						});
+			}
+		};
+	}
 }
